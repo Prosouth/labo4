8adb09e48a0e09c9886be61b722aad4a43c890f0
Sebastien Saez <monsieursaez@gmail.com>
2016-10-31 10:50:44 +0100

    added template

--- Labo4_DutuLaunay_Saez/main.cpp
+++ Labo4_DutuLaunay_Saez/main.cpp
@@ -1,25 +1,112 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.cpp
- * Author: Marion Dutu Launay
- *
- * Created on 31. octobre 2016, 10:44
- */
+/* ---------------------------
+ Laboratoire : 4
+ Fichier :     main.cpp
+ Auteur(s) :
+ Date :
+ 
+ But :         Mise en oeuvre d'opérations arithmétiques simples
+ (+,-,*) sur des entiers positifs longs représentés
+ sous forme de chaines de caractères en notation
+ décimale
+ 
+ Remarque(s) :
+ 
+ Compilateur :
+ --------------------------- */
 
 #include <cstdlib>
-
+#include <iostream>
+#include <string>
 using namespace std;
 
-/*
- * 
+/**
+ Addition
+ 
+ @param lhs entier >=0 représenté en notation décimale
+ @param rhs entier >=0 représenté en notation décimale
+ 
+ @return somme des 2 entiers représentée en notation décimale
  */
-int main(int argc, char** argv) {
+string add(string lhs, string rhs) {
+  string resultat;
+  
+  // A COMPLETER
+  
+  return resultat;
+}
 
-    return EXIT_SUCCESS;
+
+/**
+ Multiplication
+ 
+ @param lhs entier >=0 représenté en notation décimale
+ @param rhs entier >=0 représenté en notation décimale
+ 
+ @return produit des 2 entiers représenté en notation décimale
+ */
+string multiply(string lhs, string rhs) {
+  string resultat;
+  
+  // A COMPLETER
+  
+  return resultat;
+}
+
+/**
+ Factorielle
+ 
+ @param n entier >=0 
+ 
+ @return n!, la factorielle de n représentée en notation décimale
+ */
+string factorial(int n) {
+  string resultat;
+  
+  // A COMPLETER
+  
+  return resultat;
+}
+
+/**
+ Soustraction
+ 
+ @param lhs entier >=0 représenté en notation décimale
+ @param rhs entier >=0 représenté en notation décimale
+ 
+ @return différence (lhs - rhs) des 2 entiers représentée en notation décimale
+ */
+string subtract(string lhs, string rhs) {
+  string resultat;
+  
+  // A COMPLETER
+  
+  return resultat;
+}
+
+
+/**
+ Fonction principale
+ 
+ @return Mise en oeuvre d'opérations arithmétiques simples
+ (+,-,*) sur des entiers positifs longs représentés
+ sous forme de chaines de caractères en notation
+ décimale
+ */
+int main() {
+  
+  cout << "Entrez deux entiers >= 0 \n";
+  string i1, i2;
+  cin >> i1 >> i2;
+  
+  cout << i1 << " + " << i2 << " = " << add(i1,i2) << endl;
+  cout << i1 << " * " << i2 << " = " << multiply(i1,i2) << endl;
+  cout << i1 << " - " << i2 << " = " << subtract(i1,i2) << endl;
+  
+  cout << "\nEntrez un entier >= 0 \n";
+  int i3;
+  cin >> i3;
+  cout << "Factoriel(" << i3 << ") = " << factorial(i3) << endl;
+  
+  return EXIT_SUCCESS;
 }
 
